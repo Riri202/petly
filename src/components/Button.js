c52cfb1bee456f43ef6aaf9aea02d240954c345b
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-01-22 22:04:05 +0100

    chore:implement api consumption

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css'
+
+function Button({text}) {
+  return <div>
+      <button className='btn'>{text}</button>
+  </div>;
+}
+
+export default Button;
