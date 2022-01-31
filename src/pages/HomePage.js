c376f8a00c83b703fc5a65f30efa00abc3e9756b
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-01-31 23:18:03 +0100

    ft(homepage): api consumption to browse through pet types

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Cards from '../components/Cards'
 import Hero from '../components/Hero';
 import NavBar from '../components/NavBar'
+import PetSearch from '../components/PetSearch';
 
 function HomePage() {
   return <div>
       <NavBar/>
       <Hero/>
       <Cards/>
-      
+      <PetSearch/>
   </div>;
 }
 
