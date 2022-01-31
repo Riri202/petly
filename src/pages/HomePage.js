c0777a92dda80662db772971efede60da4ed2bae
Riri202 <92611151+Riri202@users.noreply.github.com>
2022-01-31 12:56:18 +0100

    Revert "ft-browse-pets-via-type"

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Cards from '../components/Cards'
 import Hero from '../components/Hero';
 import NavBar from '../components/NavBar'
-import PetTypeNav from '../components/PetTypeNav'
-
 
 function HomePage() {
   return <div>
       <NavBar/>
       <Hero/>
       <Cards/>
-      <PetTypeNav/>
       
   </div>;
 }
