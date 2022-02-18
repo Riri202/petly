be3654ca3c5668917a67304d652d70ca6a266566
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-02-18 21:38:20 +0100

    chore: set up proxy link for images

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -43,7 +43,7 @@ const useStyles = makeStyles( theme => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            width: '80vw'
+            width: '75vw'
            
           },
           avatarContainer: {
@@ -91,9 +91,10 @@ const useStyles = makeStyles( theme => {
           },
           imgCarousel: { 
             display: 'flex',
-            width: '75vw',
-            justifyContent: 'space-evenly',
+            width: '70vw',
+            justifyContent: 'flex-start',
             overflowX: 'scroll',
+            marginLeft: 20
             
            
           },
@@ -278,14 +279,14 @@ const images = {
             <Typography style={{display: 'flex', alignSelf: 'flex-start'}} variant='h4'>{pet.name} Pictures</Typography>
      
               <div className={classes.imgTop}>
-                 <img src={getLinkForproxy(pet.images[0])} alt='pet' style={{height: 247, width: '100%', borderRadius: 5, objectFit: 'cover'}} />
+                 <img src={getLinkForProxy(pet.images[0])} alt='pet' style={{height: 247, width: '100%', borderRadius: 5, objectFit: 'cover'}} />
               </div>   
             
               <div  className={classes.imgCarousel} >
                 {pet.images.map((photo, key) => {
                   return (
                     
-                      <img src={getLinkForproxy(photo)} key={key} alt='pet' style={{display: 'flex', alignSelf: 'flex-start'}} className={classes.imgCarouselItem} />
+                      <img src={getLinkForProxy(photo)} key={key} alt='pet' style={{display: 'flex', alignSelf: 'flex-start'}} className={classes.imgCarouselItem} />
                      
                   )
 
